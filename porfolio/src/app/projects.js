29fd585011a81405e54af635087518d0d6aec83e
Florencio <florencio.tom@viewnext.com>
2025-03-04 17:56:47 +0100

    .

--- porfolio/src/app/projects.js
+++ porfolio/src/app/projects.js
@@ -36,6 +36,73 @@ export const jsonProjects = [
         ],
         "tags":"React",
         "link":"https://got-dusky.vercel.app/cronologia"
+    },
+    {
+        "title":"Quien es Quien",
+        "description":"Responde a las preguntas para averiguar el avatar que se ha elegido de manera aleatoria, si hay match con tu respuesta se deshabilitaran los avatares que no encajen con tu respuesta",
+        "descriptionEn":"Answer the questions to find out which avatar has been chosen randomly. If there is a match with your answer, the avatars that do not fit your response will be disabled",
+        "images":[
+            "/quien-1.png",
+            "/quien-2.png",
+            "/quien-3.png",
+            "/quien-4.png",
+            "/quien-5.png",
+        ],
+        "tags":"React",
+        "link":"https://quienesquiendeploy.vercel.app/"
+    },
+    {
+        "title":"Rick y Morty",
+        "description":"Consulta a la api de rick y moty",
+        "descriptionEn":"Query the Rick and Morty API",
+        "images":[
+            "/rym-1.png"
+        ],
+        "tags":"React",
+        "link":"https://rickymortyapi-alpha.vercel.app/"
+    },
+    {
+        "title":"Encuentra la pareja",
+        "description":"Encuentra la pareja. Selecciona una 'x' y luego otra para ver si esconden el mismo planeta. si son el mismo planeta se revelarán",
+        "descriptionEn":"Find the pair. Select one 'X' and then another to see if they hide the same planet. If they match, they will be revealed.",
+        "images":[
+            "/planetas-1.png",
+            "/planetas-2.png"
+        ],
+        "tags":"React",
+        "link":"https://parejas-planetas.vercel.app/"
+    },
+    {
+        "title":"Encuentra el tesoro",
+        "description":"Elige la dimension del tablero y luego encuentra el tesoro en el menor número de intentos posibles",
+        "descriptionEn":"Choose the board dimension and then find the treasure in the fewest possible attempts.",
+        "images":[
+            "/tesoro-1.png",
+            "/tesoro-2.png"
+        ],
+        "tags":"React",
+        "link":"https://https-github-com-florencio-tom-encuentra-el-tesoro.vercel.app/"
+    },
+    {
+        "title":"Gift",
+        "description":"Una simple herramienta para buscar Gifts.",
+        "descriptionEn":"A simple tool for searching GIFs.",
+        "images":[
+            "/gift-1.png"
+        ],
+        "tags":"Angular",
+        "link":"https://florenciotom.github.io/react-gift-app/"
+    },
+    ,
+    {
+        "title":"Encuentra un pais",
+        "description":"Escribe el nombre de un pais y dale al enter.",
+        "descriptionEn":"Type the name of a country and press enter",
+        "images":[
+            "/sc-1.png"
+        ],
+        "tags":"Javascript",
+        "link":"https://florenciotom.github.io/search-country/"
     }
 ];
 
